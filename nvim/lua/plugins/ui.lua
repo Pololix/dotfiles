6bffa40e7a9415803a9d19dba59e4034a57bc9e4
Pololix <devpololix@gmail.com>
2026-05-02 09:59:58 +0200

    Set greyshift colorscheme

--- nvim/lua/plugins/ui.lua
+++ nvim/lua/plugins/ui.lua
@@ -1,35 +1,21 @@
 return {
     {
-        "sainnhe/everforest",
+        "Pololix/greyshift-nvim",
         lazy = false,
         priority = 1000,
         config = function()
-            vim.g.everforest_background = "hard"
-            vim.g.everforest_enable_italic = true
-            vim.g.everforest_disable_italic_comment = false
-            vim.g.everforest_cursor = "auto"
-            vim.g.everforest_ui_contrast = "low"
-            vim.g.everforest_float_style = "bright"
-            vim.g.everforest_dim_inactive_windows = false
-            vim.g.everforest_show_eob = false
-            vim.g.everforest_diagnostic_text_highlight = false
-            vim.g.everforest_diagnostic_line_highlight = false
-            vim.g.everforest_diagnostic_virtual_text = "grey"
-            vim.g.everforest_current_word = "grey background"
-            vim.g.everforest_inlay_hints_background = "none"
-            vim.g.everforest_spell_foreground = "none"
-            vim.g.everforest_better_performance = true
-            vim.g.everforest_disable_terminal_colors = false
-            vim.o.background = "dark"
-            vim.cmd.colorscheme("everforest")
+            require("greyshift").setup({})
+            vim.cmd.colorscheme("greyshift")
         end
     },
 
     {
-        "nvim-mini/mini.icons",
+        "echasnovski/mini.icons",
+        lazy = false,
         config = function()
             require("mini.icons").setup()
-            MiniIcons.mock_nvim_web_devicons()
-        end
+
+            require("mini.icons").mock_nvim_web_devicons()
+        end,
     }
 }
