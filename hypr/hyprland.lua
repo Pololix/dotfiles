ee2e7f8770750bdb2085058c2d80acd39ad19125
Pololix <devpololix@gmail.com>
2026-05-14 16:19:54 +0200

    Hypridle

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,7 +1,7 @@
 require("core/autostart/autostart")
 
+require("core/input/general")
 require("core/input/gestures")
-require("core/input/input")
 require("core/input/monitors")
 
 require("core/keymaps/keymaps")
@@ -10,3 +10,6 @@ require("core/looks/animations")
 require("core/looks/decorations")
 require("core/looks/general")
 require("core/looks/misc")
+
+require("core/rules/win-rules")
+require("core/rules/wks-rules")
