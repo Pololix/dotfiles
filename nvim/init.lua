b4207a791bf9a856448205731024e29c81370609
Pololix <devpololix@gmail.com>
2026-05-15 12:33:35 +0200

    Added linters and formatters

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,22 +3,22 @@ vim.g.maplocalleader = " "
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins", {
-	lazy = true,
-	change_detection = {
-		notify = false,
-	},
+    lazy = true,
+    change_detection = {
+        notify = false,
+    },
 })
 
 require("core.autocmds")
--- nvim/lua/plugins/format.lua
+++ nvim/lua/plugins/format.lua
@@ -1,28 +1,28 @@
 return {
-	{ -- conform
-		"stevearc/conform.nvim",
-		config = function()
-			local conform = require("conform")
-			conform.setup({
-				formatters_by_ft = {
-					lua = { "stylua" },
-				},
+    { -- conform
+        "stevearc/conform.nvim",
+        config = function()
+            local conform = require("conform")
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                },
 
-				format_on_save = function()
-					return { timeout_ms = 500, lsp_fallback = true }
-				end,
-			})
-		end,
-	},
+                format_on_save = function()
+                    return { timeout_ms = 500, lsp_fallback = true }
+                end,
+            })
+        end,
+    },
 
-	{ -- nvim-lint
-		"mfussenegger/nvim-lint",
-		config = function()
-			local lint = require("lint")
+    { -- nvim-lint
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require("lint")
 
-			lint.linters_by_ft = {
-				lua = { "luacheck" },
-			}
-		end,
-	},
+            lint.linters_by_ft = {
+                lua = { "luacheck" },
+            }
+        end,
+    },
 }
