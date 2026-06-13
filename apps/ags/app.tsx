83b3b2a4b60e22763ca86de097b2fcdec666bd0b
Pololix <devpololix@gmail.com>
2026-06-13 18:28:22 +0200

    Refactor: ags wipe

--- apps/ags/app.tsx
+++ apps/ags/app.tsx
@@ -2,7 +2,7 @@ import app from "ags/gtk4/app"
 import Wp from "gi://AstalWp"
 import { Astal } from "ags/gtk4"
 import { createPoll } from "ags/time"
-import { createBinding } from "ags"
+import { createBinding, createState } from "ags"
 
 app.start({
     main() {
@@ -34,15 +34,18 @@ function Bar() {
 function Volume() {
     const speaker = Wp.get_default().get_default_speaker()
     const volume = createBinding(speaker, "volume")
-    console.log("raw volume:", speaker.volume)
-    console.log("wp:", Wp.get_default())
-    console.log("speaker:", Wp.get_default().get_default_speaker())
+
     return <label label={volume((v) => Math.round(v * 100) + "%")} />
+}
+
+function MediaControls() {
+    const [players, setPlayers] = 
 }
 
 // Time
 function Clock() {
     const time = createPoll("", 1000, "date +%H:%M")
+
     return <label label={time} />
 }
 
