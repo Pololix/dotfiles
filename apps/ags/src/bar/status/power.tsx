23d476a56e0829e86a900ae747a895bead716800
Pololix <devpololix@gmail.com>
2026-06-14 21:40:07 +0200

    Added: battery display to ags status

--- apps/ags/src/bar/status/power.tsx
+++ apps/ags/src/bar/status/power.tsx
@@ -1,11 +1,26 @@
+import Battery from "gi://AstalBattery"
+import { createBinding } from "ags"
 import { StatusProp } from "./main"
 
 export function Power({ active, setActive }: StatusProp) {
+    const battery = Battery.get_default()
+    const percentage = createBinding(battery, "percentage")
+    const isCharging = createBinding(battery, "charging")
+
     return (
         <button cssClasses={["bar-component"]} onClicked={() => {
             setActive(active() === "power" ? null : "power")
         }}>
-            <label label="Power" />
+            <label label={percentage((v) => {
+                const icon = GetIcon(v, isCharging())
+                const num = Math.round(v * 100)
+
+                return icon + " " + num + "%"
+            })} />
         </button>
     )
+}
+
+function GetIcon(percentage, isCharging) {
+    return ""
 }
