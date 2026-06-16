6fb8c3d9a7c3c943dd72df3142074ea22d8c3002
Pololix <devpololix@gmail.com>
2026-06-16 13:38:51 +0200

    Added: volume tag

--- apps/ags/src/bar/media/volume.tsx
+++ apps/ags/src/bar/media/volume.tsx
@@ -1,5 +1,23 @@
+import Wp from "gi://AstalWp"
+import { createBinding, createComputed } from "ags"
+
 export function Volume() {
+    const device = Wp.get_default().get_default_speaker()
+
+    const percentage = createBinding(device, "volume")
+    const isMuted = createBinding(device, "mute")
+
+    const tag = createComputed(() => {
+        const m = isMuted()
+
+        if (m) return ""
+
+        const p = percentage()
+        if (p === 0) return "" + " " + Math.round(p * 100) + "%"
+        return "" + " " + Math.round(p * 100) + "%"
+    })
+
     return (
-        <label cssClasses={["bar-component"]} label="volume" />
+        <label cssClasses={["bar-component"]} label={tag} />
     )
 }
