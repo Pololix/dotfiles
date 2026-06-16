3467aa2da696954f71e460b2c1d0938c21cc0651
Pololix <devpololix@gmail.com>
2026-06-16 22:08:50 +0200

    Added: media control from mrpis

--- apps/ags/src/bar/media/controls.tsx
+++ apps/ags/src/bar/media/controls.tsx
@@ -1,19 +1,36 @@
+import mpris from "gi://AstalMpris"
+import { createState, createComputed } from "ags"
 import { GetIcon } from "../../assets/icons"
 
 export function Controls() {
+    const mprs = mpris.get_default()
+    const [players, setPlayers] = createState(mprs.get_players())
+
+    mprs.connect("items-changed", () => {
+        setPlayers(mprs.get_players)
+    })
+
+    const player = createComputed(() => players()[0])
+
     return (
         <box cssClasses={["bar-component"]} spacing={20}>
             <button onClicked={() => {
+                if (player() === undefined) return
+                player().previous()
             }}>
                 <label label={GetIcon("prev")} />
             </button>
 
             <button onClicked={() => {
+                if (player() === undefined) return
+                player().play_pause()
             }}>
                 <label label={GetIcon("play")} />
             </button>
 
             <button onClicked={() => {
+                if (player() === undefined) return
+                player().next()
             }}>
                 <label label={GetIcon("next")} />
             </button>
